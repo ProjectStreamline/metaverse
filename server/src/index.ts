f762f190383b686be019aa2cd7e780c6e46d8572
Pradnya Kumbhar <kumbharpradnyaa10@gmail.com>
2025-04-24 17:49:43 +0530

    db setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,13 @@
-import express from 'express'
-import testRoute from './routes/test.route'
-const app = express()
-import cors from "cors"
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import testRoute from './routes/test.route';
 
-app.use(cors());
+dotenv.config();
+
+const app = express();
+
+app.use(cors())
 app.use(express.json())
 
 app.use('/', testRoute)
